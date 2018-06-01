62848f82a054d1c24bd726eeee5469f29b7c2b73
Paolo D'Orsi <pa.dorsi@gmail.com>
2018-06-01 19:59:20 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+{
+    title: 'My Homepage',
+    draft: false,
+    layout: 'layout.html',
+    contents: new Buffer('Welcome to my homepage!')
+}
---
+++ src/index.md
@@ -0,0 +1,6 @@
+---
+title: My Homepage
+draft: false
+layout: layout.html
+---
+Welcome to my homepage!
